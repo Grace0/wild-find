8c67df4ee78411d59ef0f5cbff90ed9a0b14ee14
Grace0 <gracekwak25@gmail.com>
2019-12-05 14:58:32 -0800

    works but lags

--- wild-find.py
+++ wild-find.py
@@ -1,6 +1,46 @@
+# author: Arun Ponnusamy
+# website: https://www.arunponnusamy.com
+
+# object detection webcam example using tiny yolo
+# usage: python object_detection_webcam_yolov3_tiny.py
+
+# import necessary packages
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
+import cv2
 
-bbox, label, conf = cv.detect_common_objects(img)
+# open webcam
+rtsp = cv2.VideoCapture("rtsp://admin:momo@192.168.1.103/live")
 
-output_image = draw_bbox(img, bbox, label, conf)
+if not rtsp.isOpened():
+    print("Could not open webcam")
+    exit()
+
+
+# loop through frames
+while rtsp.isOpened():
+
+    # read frame from webcam
+    status, frame = rtsp.read()
+
+    if not status:
+        break
+
+    # apply object detection
+    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov3-tiny')
+
+    print(bbox, label, conf)
+
+    # draw bounding box over detected objects
+    out = draw_bbox(frame, bbox, label, conf, write_conf=True)
+
+    # display output
+    cv2.imshow("Real-time object detection", out)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release resources
+rtsp.release()
+cv2.destroyAllWindows()
