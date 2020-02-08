30fb54f7fa7593b6a8d0c66edbec1044afcd457e
Grace0 <gracekwak25@gmail.com>
2020-02-07 16:18:50 -0800

    green mask

--- concatenate_vid.py
+++ concatenate_vid.py
@@ -4,12 +4,16 @@ import os, sys
 video_clips = []
 
 root = "./vid/INSERT_FOOTAGE_HERE" #path to DAY FOLDER
+save_path = "./concated_vid/"
+
 day_list = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+day_list.sort()
 print(day_list)
 
 for day_folder in day_list:
       day_root = root + "/" + day_folder
       day_dirlist = [ item for item in os.listdir(day_root) if os.path.isdir(os.path.join(day_root, item)) ]
+      day_dirlist.sort()
       print(day_dirlist)
       for hour_folder in day_dirlist:
           hour_root = day_root + "/" + hour_folder
@@ -19,6 +23,5 @@ for day_folder in day_list:
           for minute_video in minute_list:
               clip = VideoFileClip(hour_root + "/" + minute_video)
               video_clips.append(clip)
-
 final_clip = concatenate_videoclips(video_clips)
-final_clip.write_videofile("my_concatenation.mp4")
+final_clip.write_videofile(day_folder + "_" + hour_folder + ".mp4")
--- find_movement.py
+++ find_movement.py
@@ -18,43 +18,59 @@ ret, frame2 = cap.read()
 def detect_green_boxes():
     ret, frame = cap.read()
     frame = imutils.resize(frame, width=600)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    _,threshold = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
-    contours,_=cv2.findContours(threshold, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #blurred = cv.GaussianBlur(frame, (11, 11), 0)
 
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        print(len(approx))
-        if len(approx)==4:
-            print("quadrilateral")
-            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+    greenLower = np.array([0, 100, 0], dtype="uint8") #68, 230, 38 - 28, 220, 58
+    greenUpper = np.array([100, 255, 100], dtype="uint8") #-48, 240, 78
+#    pts = deque(maxlen=args["buffer"])
+
+	# cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	# 	cv2.CHAIN_APPROX_SIMPLE)
+	# cnts = imutils.grab_contours(cnts)
+	# center = None
+
+    mask = cv.inRange(frame, greenLower, greenUpper)
+    output = cv.bitwise_and(frame, frame, mask = mask)
+
+    #_,threshold = cv.threshold(gray, 110, 255, cv.THRESH_BINARY)
+    #contours,_=cv.findContours(threshold, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow('frame', np.hstack([frame, output]))
+
+    # for cnt in contours:
+    #     approx = cv.approxPolyDP(cnt,0.01*cv.arcLength(cnt,True),True)
+    #     print(len(approx))
+    #     if len(approx)==4:
+    #         print("quadrilateral")
+    #         cv.drawContours(frame,[cnt],0,(0,0,255),-1)
+    #     cv.imshow('conts', frame)
 
 def detect_movement():
     frame1 = imutils.resize(frame1, width=600)
     frame2 = imutils.resize(frame2, width=600)
     diff = cv.absdiff(frame1, frame2)
     gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
-    _,threshold = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
-    contours,_=cv2.findContours(threshold, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    _,threshold = cv.threshold(img, 110, 255, cv.THRESH_BINARY)
+    contours,_=cv.findContours(threshold, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 
-    for cnt in contours:
 
 while cap.isOpened():
-    frame1 = imutils.resize(frame1, width=600)
-    frame2 = imutils.resize(frame2, width=600)
-    diff = cv.absdiff(frame1, frame2)
-    gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+    detect_green_boxes()
 
-    cv.imshow('frame1', frame1)
-    cv.imshow('diff', diff)
-
-    frame1 = frame2 #current becomes old
-    ret, frame2 = cap.read() #new
-
-    # time.sleep(0.5)
-
+#     frame1 = imutils.resize(frame1, width=600)
+#     frame2 = imutils.resize(frame2, width=600)
+#     diff = cv.absdiff(frame1, frame2)
+#     gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+#
+#     cv.imshow('frame1', frame1)
+#     cv.imshow('diff', diff)
+#
+#     frame1 = frame2 #current becomes old
+#     ret, frame2 = cap.read() #new
+#
+    time.sleep(0.1)
+#
     if cv.waitKey(1) == ord('q'):
-        break
+         break
 
 cap.release()
 cv.destroyAllWindows()
